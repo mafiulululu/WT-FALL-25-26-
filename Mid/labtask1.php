565534d412ab2b6e563132937520eea1be69bd50
Mahmudul Hassan Mafi <mahmudulmafi@gmail.com>
2025-11-02 12:19:34 +0600

    Client Patient

---
+++ Mid/labtask1.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Clinic Patient Registration</title>
+</head>
+<body>
+
+<h1>Clinic Patient Registration</h1>
+<p>Full Name:</p>
+<input type="text"> <br>
+<p>Age:</p>
+<input type="text"> <br>
+<p>Phone Number:</p>
+<input type="text"> <br>
+<p>Email Address:</p>
+
+<input type="text"> <br>
+
+ <td>Insurance Provider:</td>
+                <td>
+                    <select>
+                        <option>Select Provider</option>
+                        <option>Provider A</option>
+                        <option>Provider B</option>
+                        <option>Provider C</option>
+                    </select>
+
+<br>
+
+
+<p>Insurance Policy Number:</p>
+<input type="text"> <br>
+
+<h1>Additional Information</h1>
+<p> Username:</p>
+<input type="text"> <br>
+<p>Password:</p>
+<input type="text"> <br>
+<p>Confirm Password:</p>
+<input type="text"> <br>
+
+<br><br>
+<center>
+    <input type="Submit">
+</center>
